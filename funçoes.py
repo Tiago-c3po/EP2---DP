ac5a0ce0a39a92aec8437ebb72094caf528bca8e
tiago-c3po <tiagovl@al.insper.edu.br>
2023-01-26 14:54:18 -0300

    preeche frota

--- funçoes.py
+++ funçoes.py
@@ -1,11 +1,18 @@
-def define_posicoes(linha, coluna, orientação, tamanho):
+def define_posicoes(linha, coluna, orientacao, tamanho):
     posicao = []
     i=0
     while i < tamanho:
-        if orientação == 'horizontal':
+        if orientacao == 'horizontal':
             posicao.append([linha,coluna+i])
-        if orientação == 'vertical':
+        if orientacao == 'vertical':
             posicao.append([linha+i, coluna])
         i+=1
     return posicao
-print(define_posicoes(0, 0, 'horizontal', 1))
+
+def preenche_frota(frota, nome, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+    if nome not in frota:
+        frota[nome] = [posicoes]
+    else:
+        frota[nome].append(posicoes)
+    return frota
