e75fa8b614667c4f05c9778d63ec7930aef1e935
gabrielsantc <gabriels6@al.insper.edu.br>
2023-01-29 21:25:17 -0300

    corrigindo erros

--- funçoes.py
+++ funçoes.py
@@ -53,4 +53,54 @@ def posiciona_frota(frota):
                                 lista[i][j] = 1
     return lista
 
+
+def afundados(frota, tabuleiro):
+    cont = 0
+    afundados = 0
+    for posicao in frota.values():
+        for quant in posicao:
+            cont = 0 
+            for info in quant:
+                for i in range(len(tabuleiro)):
+                    if i == info[0]:
+                        for j in range(len(tabuleiro[i])):
+                            if j == info[1]:
+                                if tabuleiro[i][j] == 'X':
+                                    cont += 1
+            if len(quant) == cont:
+                afundados += 1
+    return afundados
+
+
+    
+def posicao_valida(frota, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+    valida = True
+    if len(frota) == 0:
+        for t in range(len(posicoes)):
+            if posicoes[t][0] < 9:
+                if posicoes[t][1] < 9:
+                    valida = True
+                elif posicoes [t][1] > 9:
+                    return False
+            elif posicoes[t][0]> 9 or posicoes[t][1]> 9:
+                return False
+        return valida
+
+    for p in range(len(posicoes)):
+        for posicao in frota.values():
+            for q in posicao:
+                for info in q:
+                    if posicoes[p][0] > 9:
+                        return False
+                    if posicoes[p][1] > 9:
+                        return False
+                    if posicoes[p][0] == info[0]:
+                        if posicoes[p][1] > 9:
+                            return False
+                        if posicoes[p][1] == info[1]:
+                            return False
+                        elif posicoes[p][1] != info[1]:
+                            valida = True
+    return valida
     
